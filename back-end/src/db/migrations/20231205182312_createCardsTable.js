a03c81c86430eeb12ea15729ad5dc9b32bebc9f7
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-12-05 12:28:17 -0600

    Ran migrations and they're working.

--- back-end/src/db/migrations/20231205182312_createCardsTable.js
+++ back-end/src/db/migrations/20231205182312_createCardsTable.js
@@ -7,6 +7,7 @@ exports.up = function (knex) {
     table.increments("id").primary();
     table.string("front");
     table.string("back");
+    table.integer("deckId").unsigned().notNullable();
     table
       .foreign("deckId")
       .references("id")
