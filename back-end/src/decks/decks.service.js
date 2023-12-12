2522f2af631cbafd2aff2c3d5941364427da038a
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-12-12 13:00:53 -0600

    Fixed syntax in create deck service file.

--- back-end/src/decks/decks.service.js
+++ back-end/src/decks/decks.service.js
@@ -1,7 +1,10 @@
 const knex = require("knex");
 
 function create(newDeck) {
-  return knex("decks").insert(newDeck);
+  return knex("decks")
+    .insert(newDeck)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
 }
 
 function list() {
