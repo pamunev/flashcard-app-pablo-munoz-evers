7a5c4a473799a07d1b0bfa995a6319b7adffe2d6
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-12-12 14:11:08 -0600

    Enabled cors in app.js of back-end.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 const cardsRouter = require("./cards/cards.router");
 const decksRouter = require("./decks/decks.router");
 
 // Middleware
 app.use(morgan("dev"));
+app.use(cors());
 
 // Functions
 const sayHello = (req, res, next) => {
